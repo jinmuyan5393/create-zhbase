a4301b6d25a9a3a3ba588d26220d89f6b7c82b83
jinmuyan5393 <jinmuyan5393@163.com>
2024-05-31 10:00:42 +0800

    测试admin-base基础模板

--- src/question/template/templateDate.ts
+++ src/question/template/templateDate.ts
@@ -14,52 +14,27 @@ export const templateList: TemplateList[] = [
       },
     },
   },
-
   {
-    title: `tabbar${green("(自定义tabbar)")}`,
-    description: `${red("(多TAB base + tabbar项目)")}`,
-    value: {
-      type: "tabbar",
-      branch: "tabbar",
-      url: {
-        gitee: "https://gitee.com/codercup/unibest.git",
-        github: "https://github.com/codercup/unibest.git",
-      },
-    },
-  },
-  {
-    title: `demo${green("(演示项目)")}`,
+    title: `uni-demo${green("(演示项目)")}`,
     description: `${red("(多TAB演示项目)")}`,
     value: {
-      type: "demo",
-      branch: "main",
+      type: "uni-demo",
+      branch: "demo",
       url: {
-        gitee: "https://gitee.com/codercup/hello-unibest.git",
-        github: "https://github.com/codercup/hello-unibest.git",
+        gitee: "https://github.com/jinmuyan5393/zh-uni-base.git",
+        github: "https://github.com/jinmuyan5393/zh-uni-base.git",
       },
     },
   },
   {
-    title: `i18n${green("(多语言)")}`,
-    description: `${red("(多TAB多语言项目)")}`,
+    title: `admin-base${green("(管理后台)")}`,
+    description: `${red("(管理后台基础模板)")}`,
     value: {
-      type: "i18n",
-      branch: "i18n",
+      type: "admin-base",
+      branch: "admin-base",
       url: {
-        gitee: "https://gitee.com/codercup/unibest.git",
-        github: "https://github.com/codercup/unibest.git",
-      },
-    },
-  },
-  {
-    title: `js${green("(非ts版本)")}`,
-    description: `${red("(多TAB base + js项目)")}`,
-    value: {
-      type: "js",
-      branch: "js",
-      url: {
-        gitee: "https://gitee.com/codercup/unibest.git",
-        github: "https://github.com/codercup/unibest.git",
+        gitee: "https://github.com/jinmuyan5393/zh-uni-base.git",
+        github: "https://github.com/jinmuyan5393/zh-uni-base.git",
       },
     },
   },
